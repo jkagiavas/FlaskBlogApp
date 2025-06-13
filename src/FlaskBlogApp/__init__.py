de80c1988b86aa706c039cb4ba5cd7109b13e43b
ioannis kagiavas <jkayabas@gmail.com>
2025-06-14 02:34:15 +0300

    καθαρισμα:αφαιρεσα .db και pycache απο git

--- src/FlaskBlogApp/__init__.py
+++ src/FlaskBlogApp/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+load_dotenv()
 
-app.config["SECRET_KEY"] = "6870e40e95eada2076052023834a2ce5"
-app.config['WTF_CSRF_SECRET_KEY'] = 'fe9d487ba2c9a1f13a5d72fa0d76d3fb'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['WTF_CSRF_SECRET_KEY'] = os.getenv("WTF_CSRF_SECRET_KEY")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_course_database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL", "sqlite:///flask_course_database.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
