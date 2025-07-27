e1677780b5e468d2b704f373d6377b8b7c7d28fc
ioannis kagiavas <jkayabas@gmail.com>
2025-07-28 01:15:16 +0300

    update models with category/topic/comments

--- src/FlaskBlogApp/models.py
+++ src/FlaskBlogApp/models.py
@@ -12,10 +12,31 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     profile_image = db.Column(db.String(30), default ='default_profile_image.jpg')
+
     articles = db.relationship('Article', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
 
     def __repr__(self):
         return f"{self.username}: {self.email}"
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    articles = db.relationship('Article', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f"<Category {self.name}>"
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    articles = db.relationship('Article', backref='topic', lazy=True)
+
+    def __repr__(self):
+        return f"<Topic {self.name}>"
+
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -23,8 +44,24 @@ class Article(db.Model):
     article_body  = db.Column(db.Text(), nullable=False)
     article_image = db.Column(db.String(30), default ='default_article_image.jpg')
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=True)
+    comments = db.relationship('Comment', backref='article', lazy=True)
 
     def __repr__(self):
         return f"{self.date_created}: {self.article_title}"
 
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text(), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Comment by {self.user_id} on article {self.article_id}"
